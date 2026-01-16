636c6cf6d2c4836647b64f6f95e200ba4ac87f66
tbaur <tbaur@users.noreply.github.com>
2026-01-15 21:58:54 -0800

    v3.2.7: Fix cached dimmer brightness validation error
    Fixed:
    - Fixed 'illegal value: number 0 exceeded minimum of 1' for cached dimmer accessories
      (update brightness value before setting restrictive props)

--- dist/index.js
+++ dist/index.js
@@ -38,5 +38,5 @@ Object.defineProperty(exports, "LevitonDecoraSmartPlatform", { enumerable: true,
 var platform_2 = require("./platform");
 Object.defineProperty(exports, "default", { enumerable: true, get: function () { return __importDefault(platform_2).default; } });
 // Export version
-exports.VERSION = '3.2.6';
+exports.VERSION = '3.2.7';
 //# sourceMappingURL=index.js.map
